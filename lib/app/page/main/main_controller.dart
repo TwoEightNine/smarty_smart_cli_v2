46df46c907e8f9467c387233485c757fbba40131
twoeightnine <twoeightnine@list.ru>
2020-11-29 18:24:30 +0400

    update network configs, make delayed color requests

--- lib/app/page/main/main_controller.dart
+++ lib/app/page/main/main_controller.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:async/async.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:smarty_smart_cli_v2/app/page/main/main_presenter.dart';
@@ -5,6 +8,10 @@ import 'package:smarty_smart_cli_v2/domain/entity/state.dart';
 
 class MainController extends Controller {
   final MainPresenter presenter;
+
+  static const int colorUpdateDelay = 500;
+
+  Timer _colorUpdateTimer;
   State state;
 
   MainController()
@@ -13,7 +20,11 @@ class MainController extends Controller {
 
   void loadState() => presenter.loadState();
 
-  void setLedColor(Color color) => presenter.updateLedColor(color);
+  void setLedColor(Color color) {
+    _colorUpdateTimer?.cancel();
+    _colorUpdateTimer = Timer(Duration(milliseconds: colorUpdateDelay),
+        () => presenter.updateLedColor(color));
+  }
 
   @override
   void initListeners() {
